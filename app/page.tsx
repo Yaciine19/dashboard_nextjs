c8c1b04db2d8c10fcac37bf1442da5b4b6096102
yacine <yacineddd32@gmail.com>
2025-05-21 21:15:52 +0100

    Add: Card component & data, types folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,40 @@
+import DashboardCard from "@/components/dashboard/DashboardCard";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
+
+const Cards = [
+  {
+    title: "Posts",
+    count: 100,
+    icon: <Newspaper className="text-slate-500" size={72} />
+  },
+  {
+    title: "Categories",
+    count: 12,
+    icon: <Folder className="text-slate-500" size={72} />
+  },
+  {
+    title: "Users",
+    count: 750,
+    icon: <User className="text-slate-500" size={72} />
+  },
+  {
+    title: "Comments",
+    count: 1200,
+    icon: <MessageCircle className="text-slate-500" size={72} />
+  },
+]
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-red-500">Dashboard</h1>
-      
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5 border">
+        {Cards.map((card, index) => <DashboardCard
+        key={index}
+          title={card.title}
+          count={card.count}
+          icon={card.icon}
+        />)}
+      </div>
     </>
   );
 }
